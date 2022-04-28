b02b5996822262a733c39d147d9db1cc2b08240c
Sean Pettit <s.p@mac.com>
2022-04-28 15:05:29 +0100

    first commit - working

---
+++ src/lib/ToastBarChart.svelte
@@ -0,0 +1,28 @@
+<script>
+	import '@toast-ui/chart/dist/toastui-chart.css';
+	import { onMount } from 'svelte';
+
+	let chart;
+	let container;
+
+	export let data;
+
+	const options = {
+		chart: { width: 700, height: 400 }
+	};
+
+	onMount(async () => {
+		const el = document.getElementById('chart-area');
+		const ChartImport = await import('@toast-ui/chart');
+		console.log(ChartImport);
+		const Chart = ChartImport.default;
+		console.log(Chart.barChart);
+		chart = Chart.barChart({
+			el,
+			data,
+			options
+		});
+	});
+</script>
+
+<div id="chart-area" />
---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script>
+	import ToastBarChart from '$lib/ToastBarChart.svelte';
+
+	const data = {
+		categories: ['Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+		series: [
+			{
+				name: 'Budget',
+				data: [5000, 3000, 5000, 7000, 6000, 4000, 1000]
+			},
+			{
+				name: 'Income',
+				data: [8000, 4000, 7000, 2000, 6000, 3000, 5000]
+			}
+		]
+	};
+</script>
+
+<h1>Toast Chart</h1>
+
+<ToastBarChart {data} />
